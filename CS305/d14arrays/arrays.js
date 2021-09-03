6668e5485a1edb46a67efd4e1e32a36a116a709b
mjdeitz <89864113+mjdeitz@users.noreply.github.com>
2021-09-03 16:08:04 -0500

    Update arrays.js

--- CS305/d14arrays/arrays.js
+++ CS305/d14arrays/arrays.js
@@ -96,7 +96,7 @@ function reverseArray(arr) {
 function reverseArrayInPlace(arr) {
     let arrLength = arr.length;
     for (let i = 0; i < arrLength / 2; i++) {
-        var temp = arr[i];
+        let temp = arr[i];
         arr[i] = arr[arrLength - 1 - i];
         arr[arrLength - 1 - i] = temp;
     }
