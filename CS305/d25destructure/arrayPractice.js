1118b55d0dbfcce1ef0322f8259f59f2bdd987f8
mjdeitz <89864113+mjdeitz@users.noreply.github.com>
2021-09-12 21:00:59 -0500

    update

--- CS305/d25destructure/arrayPractice.js
+++ CS305/d25destructure/arrayPractice.js
@@ -1,10 +1,10 @@
 "use strict"
 // /* eslint-disable */
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = {
-    doubleNums, doubleAges, filterEven, filterOver10, findEvenNum, findEvenAge,
-    includesEvenNum, includesEvenAge, findNumSum, findNumAvg, findMaxNum, findMaxAge
-};
+// module.exports = {
+//     doubleNums, doubleAges, filterEven, filterOver10, findEvenNum, findEvenAge,
+//     includesEvenNum, includesEvenAge, findNumSum, findNumAvg, findMaxNum, findMaxAge
+// };
 //add all of your function names here that you need for the node mocha tests
 
 
