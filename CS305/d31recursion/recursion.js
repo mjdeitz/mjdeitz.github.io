2571405d2a3a9373f13ec6a481e5b3e721de82e6
mjdeitz <89864113+mjdeitz@users.noreply.github.com>
2021-09-15 21:42:23 -0500

    update

--- CS305/d31recursion/recursion.js
+++ CS305/d31recursion/recursion.js
@@ -3,7 +3,7 @@
 /* global exports */
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
-module.exports = { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop };
+//module.exports = { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop };
 //add all of your function names here that you need for the node mocha tests
 
 /**
