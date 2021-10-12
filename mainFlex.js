10d14bb59d22e9ca420100d60e594b4e7dd6fa2b
mjdeitz <89864113+mjdeitz@users.noreply.github.com>
2021-10-12 14:19:16 -0500

    Add files via upload

---
+++ mainFlex.js
@@ -0,0 +1,27 @@
+"use strict";
+/*eslint-disable*/
+
+window.onload = function () {
+
+    const home = document.getElementById('home');
+    home.addEventListener('click', function () {
+        window.open("./index.html", '_self');
+    })
+
+    const about = document.getElementById('about');
+    about.addEventListener('click', function () {
+        window.open("./aboutMeFlex.html", '_blank');
+    })
+
+    const courses = document.getElementById('courses');
+    courses.addEventListener('click', function () {
+        window.open("./coursesFlex.html", '_blank');
+    })
+
+    const project = document.getElementById('project');
+    project.addEventListener('click', function () {
+        window.open("./projectFlex.html", '_blank');
+    })
+
+}
+
