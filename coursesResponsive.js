9ad1f6f4c9d6776480541a91cc517f46e0ad8665
mjdeitz <89864113+mjdeitz@users.noreply.github.com>
2021-10-25 22:22:14 -0500

    Update coursesResponsive.js

--- coursesResponsive.js
+++ coursesResponsive.js
@@ -4,7 +4,7 @@
 window.onload = function () {
     const home = document.getElementById('home');
     home.addEventListener('click', function () {
-        window.open("./mainResponsive.html", '_blank');
+        window.open("./index.html", '_blank');
     })
 
     const about = document.getElementById('about');
